d5d8ed6adf070b989e4a74d162f530776a04cc94
Jev <jev.kuznetsov@gmail.com>
2024-05-01 18:01:04 +0200

    remove separate mkdocs image, it is now part of dev

--- docker/mkdocs/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-source config.sh
-
-# Build the mkdocs image
-docker build -t $IMG_NAME  .
--- docker/mkdocs/demo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-source config.sh
-
-docker run -it --rm -p 8000:8000 $IMG_NAME
