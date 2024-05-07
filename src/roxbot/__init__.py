5dbb33db5c844a7f138e26f66f83f22aad5a9992
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2024-05-07 11:02:54 +0000

    add utils

--- src/roxbot/__init__.py
+++ src/roxbot/__init__.py
@@ -1,1 +1,4 @@
 from .version import __version__  # noqa F401
+
+# Define the logging format
+LOG_FORMAT = "%(asctime)s.%(msecs)03d [%(name)s] %(filename)s:%(lineno)d - %(message)s"
---
+++ src/roxbot/utils.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+utility functions
+
+Copyright (c) 2024 ROX Automation - Jev Kuznetsov
+"""
+
+import asyncio
+import logging
+import os
+from typing import Any, Coroutine
+
+import coloredlogs
+
+from roxbot import LOG_FORMAT
+
+
+def run_main_async(coro: Coroutine[Any, Any, None]) -> None:
+    """convenience function to avoid code duplication"""
+    loglevel = os.environ.get("LOGLEVEL", "INFO").upper()
+    coloredlogs.install(level=loglevel, fmt=LOG_FORMAT)
+    logging.info(f"Log level set to {loglevel}")
+
+    # silence some loggers
+    if loglevel == "DEBUG":
+        for name in ["GpsNode", "Mock_GPS", "websockets.server"]:
+            logging.warning(f"Forcing {name} log level to INFO")
+            logging.getLogger(name).setLevel(logging.INFO)
+
+    try:
+        asyncio.run(coro)
+    except KeyboardInterrupt:
+        pass
+    except ExceptionGroup as group:
+        logging.error("ExceptionGroup caught")
+        for e in group.exceptions:
+            logging.exception(f"Caught exception: {e}", exc_info=e)
+    except asyncio.CancelledError:
+        logging.error("Cancelled")
+
+    except Exception as e:
+        logging.error(f"Crashed with {e},  type: {type(e)}")
