557ec050b693ee4362f49367679f146fc2d31414
Jev <jev.kuznetsov@gmail.com>
2024-06-11 14:24:43 +0000

    refactor

--- src/roxbot/gps/mock.py
+++ src/roxbot/gps/mock.py
@@ -30,30 +30,32 @@ class Pose:
 
 class MockGPS:
     def __init__(self) -> None:
-        self.mqtt_cfg = MqttConfig()
-        self.log = logging.getLogger("mock_gps")
-        self.current_pose = Pose(0.0, 0.0, 0.0)
-        self.mqtt_adapter = MqttAdapter()
+        self.pose = Pose(0.0, 0.0, 0.0)
+
+        self._mqtt_cfg = MqttConfig()
+        self._log = logging.getLogger("mock_gps")
+
+        self._mqtt_adapter = MqttAdapter()
 
     def set_pose(self, args: dict) -> None:
         """example callback function"""
-        self.log.info(f"Setting pose to {args=}")
+        self._log.info(f"Setting pose to {args=}")
 
         if not isinstance(args, dict):
-            self.log.error("Invalid argument type, must be dict")
+            self._log.error("Invalid argument type, must be dict")
             return
 
         try:
-            self.current_pose.x = args["x"]
-            self.current_pose.y = args["y"]
-            self.current_pose.theta = args["theta"]
+            self.pose.x = args["x"]
+            self.pose.y = args["y"]
+            self.pose.theta = args["theta"]
         except KeyError as e:
-            self.log.error(f"Missing key {e}")
+            self._log.error(f"Missing key {e}")
 
     async def main(self) -> None:
         async with asyncio.TaskGroup() as tg:
-            tg.create_task(self.mqtt_adapter.main())
-            await self.mqtt_adapter.register_callback(SET_POSE_TOPIC, self.set_pose)
+            tg.create_task(self._mqtt_adapter.main())
+            await self._mqtt_adapter.register_callback(SET_POSE_TOPIC, self.set_pose)
 
 
 if __name__ == "__main__":
--- tests/test_mock_gps.py
+++ tests/test_mock_gps.py
@@ -6,12 +6,12 @@ def test_set_pose() -> None:
 
     mock = MockGPS()
 
-    assert mock.current_pose.x == 0.0
-    assert mock.current_pose.y == 0.0
-    assert mock.current_pose.theta == 0.0
+    assert mock.pose.x == 0.0
+    assert mock.pose.y == 0.0
+    assert mock.pose.theta == 0.0
 
     mock.set_pose({"x": 1.0, "y": 2.0, "theta": 3.0})
 
-    assert mock.current_pose.x == 1.0
-    assert mock.current_pose.y == 2.0
-    assert mock.current_pose.theta == 3.0
+    assert mock.pose.x == 1.0
+    assert mock.pose.y == 2.0
+    assert mock.pose.theta == 3.0
