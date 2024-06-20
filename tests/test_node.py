ff97837cd27100dcf295b7289bdbffc97fd97f66
Jev <jev.kuznetsov@gmail.com>
2024-06-20 18:09:51 +0000

    Refactor test_node.py to use custom node class for testing

--- tests/test_node.py
+++ tests/test_node.py
@@ -1,4 +1,3 @@
-# type: ignore
 import pytest
 import asyncio
 import os
@@ -9,8 +8,19 @@ if os.environ.get("CI"):
     pytestmark = pytest.mark.skip(reason="Can't run in CI.")
 
 
+class MyNode(Node):
+    """custom node"""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._coros.append(self._on_init)
+
+    async def _on_init(self) -> None:
+        self._log.info("Running custom init coroutine")
+
+
 @pytest.mark.asyncio
-async def test_node_instance():
+async def test_node_instance() -> None:
     node = Node()
     task = asyncio.create_task(node.main())
 
@@ -23,3 +33,15 @@ async def test_node_instance():
     await asyncio.sleep(1)
     await node.stop()
     await task  # Ensure the main task is awaited to finish cleanly
+
+
+@pytest.mark.asyncio
+async def test_custom_node_instance() -> None:
+    node = MyNode()
+    task = asyncio.create_task(node.main())
+
+    # Allow some time for the node to start
+    await asyncio.sleep(0.1)
+
+    await node.stop()
+    await task
