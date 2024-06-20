4870e5ca8fee3970f6eee70465bc7609e79147a1
Jev <jev.kuznetsov@gmail.com>
2024-06-20 14:07:43 +0000

    fix version (copy-paste error)

--- src/roxbot/__init__.py
+++ src/roxbot/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = version("fixposition")
+    __version__ = version("roxbot")
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "0.0.0+unknown"
 
